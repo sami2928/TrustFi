5d8d24913da89e851bd7be668367a8f2bf8ff0de
sami2928 <samiulhaq2928@hotmail.com>
2024-11-14 22:20:32 +0500

    Added Balance and Chart Layouts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Sami" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subText="Access and manage your account and transactions efficiently."
+          />
+        </header>
 
-export default Home
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+        ></TotalBalanceBox>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
